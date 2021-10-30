5f6d2754462129d2eda883b8b0d5e494dba7d91f
evgeniashennikova <evgeniashenikova@gmail.com>
2021-10-31 01:30:53 +0300

    дз 2, тест формы для студентов

--- src/test/java/formTest/PracticeForm.java
+++ src/test/java/formTest/PracticeForm.java
@@ -1,15 +1,18 @@
 package formTest;
 
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.Keys;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class PracticeForm {
 
@@ -21,35 +24,61 @@ public class PracticeForm {
     @Test
     void studentsForm() {
 
-        String specName = "Biology";
-        String shortSpecName = specName.substring(0,specName.length()-1);
+        String nameStudent = "Student1";
+        String lastName = "Ivanov";
+        String userEmail = "Student1@example.com";
+        String gender = "Male";
+        String userNumber = "8920000000";
+        String dateOfBirth = "05 Oct 2000";
+        String specName = "Hindi";
+        String shortSpecName = specName.substring(0, specName.length() - 1);
         String subjectsContainerXpath = "//div[@id='subjectsContainer']";
-        String specNameXpath = String.format("//div[text()='%s']",specName);
-        String specialization = subjectsContainerXpath+specNameXpath;
+        String specNameXpath = String.format("//div[text()='%s']", specName);
+        String specialization = subjectsContainerXpath + specNameXpath;
+        String hobbies = "Sports";
+        String image = "src/test/resources/cat.jpg";
+        String imageName = "cat.jpg";
+        String address = "SPb";
+        String state = "NCR";
+        String city = "Noida";
+
 
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Student1");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("Student1@example.com");
-        $("[for=gender-radio-1]").click(); //Почему не срабатывает через selectRadio("Male")
-        $("#userNumber").setValue("8920000000");
+        $("#firstName").setValue(nameStudent);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
         $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "A");
-        $("#dateOfBirthInput").sendKeys("05 Oct 2000" + Keys.ENTER);
+        $("#dateOfBirthInput").sendKeys(dateOfBirth + Keys.ENTER);
         $("#subjectsInput").click();
         $("#subjectsInput").sendKeys(shortSpecName);
         $x(specialization).click();
-        $("[for=hobbies-checkbox-1]").click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/cat.jpg"));
-        $("#currentAddress").setValue("SPb");
+        $(byText(hobbies)).click();
+        $("#uploadPicture").uploadFile(new File(image));
+        $("#currentAddress").setValue(address);
         $("#state").scrollTo();
         $("#state").click();
-        $x("//div[@id='state']//div[text()='Uttar Pradesh']").shouldBe(Condition.visible).click();
+        $(byText(state)).click();
         $("#city").click();
-        $x("//div[@id='city']//div[text()='Agra']").click();
+        $(byText(city)).click();
         $("#submit").click();
 
-
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").
+                shouldHave(text(nameStudent)).
+                shouldHave(text(lastName)).
+                shouldHave(text(userEmail)).
+                shouldHave(text(gender)).
+                shouldHave(text(userNumber)).
+                shouldHave(text("05 October,2000")).
+                shouldHave(text(specName)).
+                shouldHave(text(hobbies)).
+                shouldHave(text(imageName)).
+                shouldHave(text(address)).
+                shouldHave(text(state)).
+                shouldHave(text(city));
+        $("#closeLargeModal").shouldBe();
     }
-
 }
